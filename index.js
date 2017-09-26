558fdbad05d113c05cf30a35e98ca4845c52ca4b
Ricardo Junior <eu@ricardojr.com.br>
2017-09-26 18:26:42 -0300

    Remove unecessary dependencie from commander (its more simple than this)

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
 #!/usr/bin/env node
-const program = require('comander');
-
-program
-    .version('0.0.1')
-    .description('A new git flow with hooks for waffle.io. And now you have a Waffle with honey.');
-
-program
-    .command('commit')
-    .description('Trigger ghooks for commit')
-    .action(() => {
-        require('./gitValidations.js');
-    });
-
-program
-    .command('rebase')
-    .description('Trigger ghooks for rebase')
-    .action(() => {
-        require('./gitValidations.js');
-    });
-
-program.parse(process.argv);
+require('./gitValidations.js')
